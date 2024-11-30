55862da25f0bdc3ffed86ea92b98b058e6b4c550
Ritik Mishra <ritikmishra23april@gmail.com>
2024-11-30 23:40:50 +0530

    linear

--- Searching/linear.java
+++ Searching/linear.java
@@ -1,5 +1,8 @@
 package Searching;
 
+// A linear search is the simplest approach employed to search for an element in a data set. 
+// It examines each element until it finds a match, starting at the beginning of the data set, until the end. 
+// The search is finished and terminated once the target element is located
 public class linear {
     public static int linearSearch(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
@@ -11,9 +14,9 @@ public class linear {
     }
 
     public static void main(String a[]) {
-        int[] a1 = { 10, 20, 30, 50, 70, 90 };
+        int[] a = { 10, 20, 30, 50, 70, 90 };
         int key = 50;
-        System.out.println(key + " is found at index: " + linearSearch(a1, key));
+        System.out.println(key + " is found at index: " + linearSearch(a, key));
     }
 
 }
